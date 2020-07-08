9c34b5327f5400d691e15115f2662ec9e67545b6
Sergei <snorvaishas@yandex.ru>
2020-07-08 12:07:00 +0300

    create element update

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React from 'react';
-// import './App.css';
-
-let companyName = 'Facebook';
-let year = 2013;
-const qurentYear = new Date().getFullYear();
 
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        Этот элемент надо было удалить?
-      </header>
-      
-      <div>
-        <ul style = {
-          {textAlign: 'left',
-          color: 'darkblue'}}>
-        </ul>
-      </div>
-    </div>
-  );
-}
+const Elem = (<h1
+    style = {{textAlign: 'center'}}>Hello!!!</h1>);
 
-function MyElement() {
-  return (
-    <h1>My Element!</h1>
-  );
-}
 
-export {App, MyElement };
-// export default App;
+export default Elem;
