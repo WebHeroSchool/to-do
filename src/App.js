d13b9bac4fa94e0ee4a3e72fa9147a41ce7cf681
Sergei <snorvaishas@yandex.ru>
2020-07-08 09:22:40 +0300

    jsx training

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let companyName = 'Facebook';
+let year = 2013;
+const qurentYear = new Date().getFullYear();
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Отредактируйте и сохраните измекнения в  <code>src/App.js</code> для перезагрузки.
+          Отредактируйте и сохраните изменения в  для перезагрузки.
         </p>
         <a
           className="App-link"
@@ -16,9 +21,28 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <img src = 'https://www.meme-arsenal.com/memes/47b8bbf0f3049843bb868f1addbcedc5.jpg' className = 'learn'/>
+          <img 
+            src = 'https://www.meme-arsenal.com/memes/47b8bbf0f3049843bb868f1addbcedc5.jpg'
+            className = 'learn'
+          />
         </a>
       </header>
+      <div>
+        <ul style = {
+          {textAlign: 'left',
+          color: 'darkblue'}}>
+
+          <li>React был создан в {companyName} в {year} году, {qurentYear - 2013} лет назад</li>
+          <li>{companyName && 'Да'}</li>
+          <li>{companyName === companyName ? 'Да' : 'Нет'}</li>
+          <li>{companyName === "VK" ? 'Да' : 'Нет'}</li>
+          
+          {year = undefined}
+          {companyName = null}
+
+          <li>{year, companyName, false, true}</li>
+        </ul>
+      </div>
     </div>
   );
 }
