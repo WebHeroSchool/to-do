1ec8dcd0855b6958cef7f7b0b101f886769e9424
Sergei <snorvaishas@yandex.ru>
2020-07-17 02:44:55 +0300

    codacy bug fix

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import css from './InputItem.module.css'
+import css from './InputItem.module.css';
 
-const InputItem = () =>(
+const InputItem = () => (
   <input
     placeholder="You can add new task here"
-    className={css.main}
+    className = {css.main}
   />);
 
 export default InputItem;
