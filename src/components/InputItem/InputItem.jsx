081b6acd96047676eac838bdc7c3991e4e14498e
Sergei <snorvaishas@yandex.ru>
2020-07-17 02:36:39 +0300

    lesson 23. CSS Modules

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import css from './InputItem.module.css'
 
-const InputItem = () => <input placeholder="You can add new task here"/>;
+const InputItem = () =>(
+  <input
+    placeholder="You can add new task here"
+    className={css.main}
+  />);
 
 export default InputItem;
