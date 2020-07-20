e2ccbc339aa7b62e1df58c8cb045fc994d2fda5f
Sergei <snorvaishas@yandex.ru>
2020-07-20 18:47:15 +0300

    codacy fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   }
 
   markAsCompleted(statusFromItem) {
-    console.log(statusFromItem)
+    console.log(statusFromItem);
   }
 
 
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -4,11 +4,11 @@ import css from './Item.module.css';
 import Checkbox from '@material-ui/core/Checkbox';
 
 const Item = ({task, status, onClick}) => (
-  <li
-    className={classnames(
-      {[css.active]: true, [css.done]: status,}
-    )
-    }>
+  <li className={
+    classnames({
+      [css.active]: true,
+      [css.done]: status,})
+  }>
     <Checkbox onClick={() => onClick(status)} />
     {task}
   </li>
