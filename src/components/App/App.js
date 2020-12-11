034187f1b0ec22cac1a1ca821a4576b0dacf7ebe
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-12-11 06:44:27 +0300

    lesson 32. add items, use forms and controled components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,27 +5,32 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends Component {
+
+  taskId = 4;
+
   state = {
     items: [
-      {
-        value: 'Сверстать сайт с помощью HTML5 и CSS3',
-        isDone: true,
-        id: 1,
-      },
-      {
-        value: 'Написать игру на JS',
-        isDone: true,
-        id: 2,
-      },
-      {
-        value: 'Создать to-do приложение используюя React',
-        isDone: false,
-        id: 3,
-      },
+      { value: 'Сверстать сайт с помощью HTML5 и CSS3', isDone: true, id: 1 },
+      { value: 'Написать игру на JS', isDone: true, id: 2 },
+      { value: 'Создать to-do приложение используюя React', isDone: false, id: 3 },
     ]
   };
 
-  removeItem = (itemId) => {
+  addItem = (label) => {
+
+    const newItem = {
+      value: label,
+      isDone: false,
+      id: this.taskId++
+    };
+
+    this.setState(({items}) => {
+      const updatedItems = [...items, newItem];
+      return {items: updatedItems};
+    });
+  };
+
+  deleteItem = (itemId) => {
     const {items} = this.state;
     const remainedItems = items.filter(item => item.id !== itemId);
 
@@ -59,11 +64,11 @@ class App extends Component {
     return (
       <div className = {styles.wrap}>
         <h1 className = {styles.title} >My to-do list</h1>
-        <InputItem/>
+        <InputItem addItem={this.addItem}/>
         <ItemList
           items={items}
           toggleStatus={this.toggleStatus}
-          onDelete={this.removeItem}
+          onDelete={this.deleteItem}
         />
         <Footer remainder = {count}/>
       </div>
--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -1,18 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import style from './InputItem.module.css';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () => (
-  <div className= {style.wrap}>
-    <input
-      placeholder="You can add new task here"
-      className = {style.main}
-    />
-    <Button variant="contained" color="primary" href="#contained-buttons">
-      Add
-    </Button>
-  </div>
-);
+export default class InputItem extends Component {
 
+  state = {
+    inputValue: '',
+    error: false
+  };
 
-export default InputItem;
+  changeInputValue = (e) => {
+    const inputValue = e.target.value.toUpperCase()
+    this.setState({
+      inputValue
+    });
+  };
+
+  formSubmit = (e) => {
+    e.preventDefault();
+
+    if (this.state.inputValue === '') {
+      this.setState({
+        error: true
+      });
+    }
+    else {
+      this.props.addItem(this.state.inputValue);
+      this.setState({
+        inputValue: '',
+        error: false
+      });
+    }
+  };
+
+  render() {
+    const {inputValue, error} = this.state;
+    const errorText = error ? 'ERROR' : 'You can add new task here';
+    const helpText = error ? 'This field cannot be empty.' : ' ';
+
+    return (
+      <form className={style.wrap}
+            onSubmit={this.formSubmit}>
+        <TextField
+          value={inputValue}
+          error = {error}
+          helperText={helpText}
+          label={errorText}
+          onChange={this.changeInputValue}
+          fullWidth
+          variant="outlined"
+        />
+        <Button type='submit' variant="contained" color="primary">
+          Add
+        </Button>
+      </form>
+    );
+  };
+};
