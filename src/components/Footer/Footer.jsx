e07495f5768df7979c3de1a30a99e12ee4ee7863
Sergei Norvaishas <snorvaishas@yandex.ru>
2020-12-11 21:10:33 +0300

    lesson 33. defaultProps

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import style from './Footer.module.css';
 
-const Footer = ({remainder}) => (
-  <p className={style.text}>Tasks left: {remainder}</p>);
+const Footer = ({remainder}) => <p className={style.text}>Tasks left: {remainder}</p>
+
+Footer.defaultProps = {
+  remainder: 0
+};
 
 export default Footer;
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,31 +5,34 @@ import Checkbox from '@material-ui/core/Checkbox';
 import {DeleteForever} from '@material-ui/icons';
 
 const Item = ({id, task, status, toggleStatus, onDelete, indexInList}) => {
-    return (
-      <>
-          <li className={
-              classnames({
-                  [styles.active]: true,
-                  [styles.done]: status
-              })
-          }>
-              <Checkbox
-                color='primary'
-                checked={status}
-                onChange={() => toggleStatus(indexInList)}
-              />
-              {task}
-              <span className={styles.hide}>
-                  <DeleteForever
-                    className={styles.icon}
-                    onClick={() => onDelete(id)}
-                    style={{ color: 'darkorange' }}
-                  />
-              </span>
-          </li>
+  return (
+    <li className={
+      classnames({
+        [styles.active]: true,
+        [styles.done]: status
+      })
+    }>
+      <Checkbox
+        color='primary'
+        checked={status}
+        onChange={() => toggleStatus(indexInList)}
+      />
+      {task}
+      <span className={styles.hide}>
+        <DeleteForever
+          className={styles.icon}
+          onClick={() => onDelete(id)}
+          style={{color: 'darkorange'}}/>
+        </span>
+    </li>
+  )
+};
 
-      </>
-    )
+Item.defaultProps = {
+  status: false,
+  task: 'Сделать что-что',
+  toggleStatus: () => {},
+  onDelete: () => {}
 };
 
 export default Item;
